b8f105281ad41302158243b0cd086712bf86a5ff
Yashraj_K <yashrajkamble7727@gamil.com>
2024-08-03 18:16:22 +0530

    small change in playvideo file

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -48,7 +48,7 @@ const PlayVideo = () => {
     return (
         <div className='play-video'>
             <iframe
-                src={`https://www.youtube.com/embed/${videoId}?autoplay=0&mute=1`}
+                src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0`}
                 frameBorder="0"
                 allow="accelerometer; autoplay;  clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
